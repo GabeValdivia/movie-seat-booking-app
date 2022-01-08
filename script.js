706ffa84b1f242d5487f1162aa1f2c6159494099
GabeValdivia <gabe@gabeswebcoding.com>
2022-01-07 22:51:06 -0600

    Write function to update total and count

--- script.js
+++ script.js
@@ -6,9 +6,21 @@ const movieSelect = document.getElementById('movie');
 
 const ticketPrice = +movieSelect.value;
 
+//Update total and count
+function updateSelectedCount() {
+	const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+	const selectedSeatsCount = selectedSeats.length;
+
+	count.innerText = selectedSeatsCount;
+	total.innerText = selectedSeatsCount * ticketPrice;
+}
+
 //EventListeners
 container.addEventListener('click', e => {
 	if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
 		e.target.classList.toggle('selected');
+
+		updateSelectedCount();
 	}
 })
